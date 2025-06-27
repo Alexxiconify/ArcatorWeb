0f302b2c4d2f963f498e94a9127e40ed004590c2
Alex <89748403+Alexxiconify@users.noreply.github.com>
2025-06-27 12:10:21 -0600

    v0.0.4.453

--- navbar.js
+++ navbar.js
@@ -432,12 +432,6 @@ const navbarTemplate = `
         </svg>
         <span>Forms</span>
       </a>
-      <a class="navbar-link" href="donations.html">
-        <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.367 2.684 3 3 0 00-5.367-2.684z"></path>
-        </svg>
-        <span>Discord</span>
-      </a>
     </div>
 
     <!-- User profile section -->
@@ -725,8 +719,7 @@ export async function loadFooter(yearElementId = null, additionalLinks = []) {
 
   // Default footer links
   const defaultLinks = [
-    {href: 'privacy.html', text: 'Privacy Policy'},
-    {href: 'terms.html', text: 'Terms of Service'},
+    {href: 'privacy.html', text: 'Legal'},
     {href: 'https://wiki.arcator.co.uk/', text: 'Wiki', external: true},
     {href: 'infrastructure.html', text: 'Infrastructure'},
     {href: 'admin_and_dev.html', text: 'Admin & Dev'}
