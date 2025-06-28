47f6d97554cd8842a7d747e20b31e86191a89f2d
Alex <89748403+Alexxiconify@users.noreply.github.com>
2025-06-28 09:55:07 -0600

    v0.0.4.535

--- navbar.js
+++ navbar.js
@@ -565,11 +565,11 @@ const navbarTemplate = `
         </svg>
         <span>Games</span>
       </a>
-      <a class="navbar-link" href="infrastructure.html">
+      <a class="navbar-link" href="forms.html">
         <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v4a1 1 0 001 1h3m10-5v4a1 1 0 01-1 1h-3m-4 4v4a1 1 0 001 1h3m10-5v4a1 1 0 01-1 1h-3"></path>
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"></path>
         </svg>
-        <span>Infrastructure</span>
+        <span>Forum</span>
       </a>
       <a class="navbar-link" href="admin_and_dev.html">
         <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
